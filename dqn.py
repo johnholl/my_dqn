fix target frequency, fix input scaling

dqn.py
@@ -58,9 +58,10 @@ class DQN:
         self.loss = tf.reduce_mean(tf.square(tf.sub(self.action_readout, self.target)))
         self.optimizer = tf.train.RMSPropOptimizer(0.00025, decay=0.95, epsilon=0.01)
         self.gradients_and_vars = self.optimizer.compute_gradients(self.loss)
+        self.gradients = [gravar[0] for gravar in self.gradients_and_vars]
+        self.gradient_avgs = [tf.reduce_mean(grads) for grads in self.gradients]
         self.clipped_gradients = [(tf.clip_by_value(gv[0], -1., 1.), gv[1]) for gv in self.gradients_and_vars]
         self.train_operation = self.optimizer.apply_gradients(self.clipped_gradients)
-        # self.train_operation = tf.train.RMSPropOptimizer(0.00025, decay=0.95, epsilon=0.01).minimize(self.loss)
         self.sess.run(tf.initialize_all_variables())
 
 

test_environment.py
@@ -1,5 +1,7 @@
 from atari_env import Environment
 import random
+from observation_processing import preprocess
+import numpy as np
 
 env = Environment(rom="/home/john/code/pythonfiles/my_dqn/Breakout.bin")
 obs = env.reset()
@@ -8,6 +10,6 @@ while not done:
     #action = random.choice([0,1,2,3])
     action = raw_input("enter an action")
     obs, rew, done = env.step(action)
-    print(rew)
-    # raw_input("Press Enter to continue...")
-    env.render(0.01)
+    obs = preprocess(obs)
+    print(np.max(obs))
+    print(np.min(obs))

train.py
@@ -1,7 +1,6 @@
 from dqn import DQN
 from observation_processing import preprocess
 import numpy as np
-import tensorflow as tf
 import random
 
 
@@ -17,7 +16,7 @@ weight_average_array = []
 loss_vals = []
 episode_number = 0
 
-while total_steps < 10000000:
+while total_steps < 20000000:
     obs1 = dqn.env.reset()
     obs2 = dqn.env.step(dqn.env.sample_action())[0]
     obs3 = dqn.env.step(dqn.env.sample_action())[0]
@@ -53,21 +52,20 @@ while total_steps < 10000000:
 
             states = [m[0] for m in minibatch]
             feed_dict = {dqn.input: states, dqn.target: target_q, dqn.action_hot: action_list}
-            _, loss_val = dqn.sess.run(fetches=(dqn.train_operation, dqn.loss), feed_dict=feed_dict)
+            _, loss_val, grad_avgs = dqn.sess.run(fetches=(dqn.train_operation, dqn.loss, dqn.gradient_avgs), feed_dict=feed_dict)
             loss_vals.append(loss_val)
 
-        if total_steps % 10000 == 0:
+        if total_steps % 40000 == 0:
             target_weights = dqn.sess.run(dqn.weights)
 
         if total_steps % 50000 == 0:
             weight_avgs, avg_Q, avg_rewards, max_reward, avg_steps = dqn.test_network()
-            learning_data.append([total_steps, avg_Q, avg_rewards, max_reward, avg_steps, np.mean(loss_vals[-100]), prob])
+            learning_data.append([total_steps, avg_Q, avg_rewards, max_reward, avg_steps,
+                                  np.mean(loss_vals[-100]), prob, grad_avgs])
             weight_average_array.append(weight_avgs)
             np.save('learning_data', learning_data)
             np.save('weight_averages', weight_average_array)
-
-        if total_steps % 500000 == 0:
-            np.save('weights_' + str(int(total_steps/500000)), target_weights)
+            np.save('weights_' + str(int(total_steps/50000)), target_weights)
 
         total_steps += 1
         steps += 1